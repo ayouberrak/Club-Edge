dbda295cd3fc192bc9fde1b5c2c6125060e03c9c
reda-guemry <guemryreda@gmail.com>
2026-01-19 20:01:39 +0000

    add the classes of loger rest probleme

--- public/index.php
+++ public/index.php
@@ -1,14 +1,25 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 require_once __DIR__ .'/../core/helpers.php';
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Core\Router;
-define('BASE_URL', '/coachPro_v3');
+use core\Router;
+use core\ErrorHandler ; 
+
+ErrorHandler::register() ;
+
+define('BASE_URL', '/Club-Edge');
+
+
 $router = new Router();
 
 require_once __DIR__ . '/../routes/web.php';
+$testobj = new controller ; 
 
 $router->dispatch();
 
