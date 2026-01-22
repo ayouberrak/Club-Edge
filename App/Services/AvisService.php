e64adc1b69df4e8ef45d6b7820a3a0b245284b53
reda-guemry <guemryreda@gmail.com>
2026-01-22 10:32:50 +0000

    add participent repository

--- App/Services/AvisService.php
+++ App/Services/AvisService.php
@@ -1,31 +1,49 @@
-<?php 
+<?php
 
 namespace App\Services;
 
-use App\Repository\AvisRepository; 
+use App\Repository\AvisRepository;
 
 
-class AvisService {
+class AvisService
+{
 
-    public function findAvis() {
-        
+    public function findAvis($data)
+    {
+
+        $avisRepo = new AvisRepository();
+
+        return $avisRepo->findbyid($data);
+
     }
-    
 
-    public function addAvis($CommentData) {
+
+    public function addAvis($CommentData)
+    {
         $requiredFields = ['note', 'commentaire', 'id_user', 'id_event'];
 
         foreach ($requiredFields as $field) {
             if (!isset($CommentData[$field]) || empty($CommentData[$field])) {
                 return [
-                    'success' => false, 
+                    'success' => false,
                     'message' => "The field '$field' cannot be empty."
                 ];
             }
         }
 
-        $avisRepo = new AvisRepository() ; 
-        $avisRepo -> create($CommentData) ;
+        $userId = ['id_user' => $CommentData['id_user']];
+
+        $participent = $this->findAvis($userId);
+
+        if (!$participent) {
+            return [
+                'success' => false,
+                'message' => "Vous devez participer avant de donner votre avis."
+            ];
+        }
+
+        $avisRepo = new AvisRepository();
+        $avisRepo->create($CommentData);
 
     }
 
