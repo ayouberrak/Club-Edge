ffd9d02fc478093a853a8a8a90721344c77a11fd
reda-guemry <guemryreda@gmail.com>
2026-01-22 10:17:30 +0000

    add repository avis

--- App/Services/AvisService.php
+++ App/Services/AvisService.php
@@ -1,15 +1,31 @@
 <?php 
 
-namespace App\Services; 
+namespace App\Services;
+
+use App\Repository\AvisRepository; 
 
 
 class AvisService {
+
+    public function findAvis() {
+        
+    }
     
 
     public function addAvis($CommentData) {
-        
+        $requiredFields = ['note', 'commentaire', 'id_user', 'id_event'];
 
+        foreach ($requiredFields as $field) {
+            if (!isset($CommentData[$field]) || empty($CommentData[$field])) {
+                return [
+                    'success' => false, 
+                    'message' => "The field '$field' cannot be empty."
+                ];
+            }
+        }
 
+        $avisRepo = new AvisRepository() ; 
+        $avisRepo -> create($CommentData) ;
 
     }
 
